4e9289725d5a619d7a707531c88d6db4756db73d
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-09 20:34:11 +0300

    commit tree 9023bab82016fdc961ca4af6542f96b1f353e74e

---
+++ index.js
@@ -0,0 +1,19 @@
+
+//divide
+const divide = (num3= 2000, num4 = 100) => num3 / num4
+console.log(divide());
+
+// square
+const square = x => x**2
+console.log(square(2));
+
+/*let nums = [2, 2];
+let square = (base, exponent) => { return Math.pow(base, exponent);};
+console.log(square(2, 2));*/
+
+//add
+const add = (a, b) =>(a + b)
+console.log(add(2,4));
+
+
+
